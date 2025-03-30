4fcf4c214e72fef37494182fa88c522c371f502f
9309800659 <vishalp123556@gmail.com>
2025-03-30 18:51:54 +0530

    how

---
+++ Test/src/com/velocity/Velocity3.java
@@ -0,0 +1,20 @@
+package com.velocity;
+
+public class Velocity3 {
+	
+	
+	public static void main(String[] args) {
+		for(int i = 0; i<10; i++) {
+			System.out.println(i);
+			System.out.println("Kundan");
+			System.out.println("Vishal");
+			System.out.println("Kundan");
+			System.out.println("Vishal");
+		
+			
+			
+			
+		}
+	}
+
+}
